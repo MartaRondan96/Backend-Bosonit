9a128bc6b51db58d13acf2510081f935ceadefe0
MartaRondan96 <martarondan96@gmail.com>
2023-04-17 18:16:28 +0200

    application.properties

--- block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
@@ -1,13 +1,27 @@
 package com.example.block5properties;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Block5PropertiesApplication {
+public class Block5PropertiesApplication implements CommandLineRunner {
+	@Value("${greeting}")
+	public String greeting;
+	@Value("${my.number}")
+	public int number;
+	@Value("${new.property: ‘new.property no tiene valor'}")
+	public String newProperty;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Block5PropertiesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("El valor de greeting: "+greeting);
+		System.out.println("El valor de number: "+number);
+		System.out.println("El valor de number: "+newProperty);
+	}
 }
