859221a834467a8921bf4e0200784bbadcdb6c08
MartaRondan96 <martarondan96@gmail.com>
2023-04-18 10:48:28 +0200

    Block5-properties Segundo apartado

--- block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
@@ -1,12 +1,18 @@
 package com.example.block5properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Block5PropertiesApplication implements CommandLineRunner {
+
+	@Autowired
+	public Environment environment;
+	public String prueba;
 	@Value("${greeting}")
 	public String greeting;
 	@Value("${my.number}")
@@ -24,5 +30,7 @@ public class Block5PropertiesApplication implements CommandLineRunner {
 		System.out.println("El valor de greeting: "+greeting);
 		System.out.println("El valor de number: "+number);
 		System.out.println("El valor de new.property: "+newProperty);
+		this.prueba = environment.getProperty("greeting");
+		System.out.println(prueba);
 	}
 }
