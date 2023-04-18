be0574354d89997d9cd615dcffbe1da42010e035
MartaRondan96 <martarondan96@gmail.com>
2023-04-18 17:57:55 +0200

    Block5-PROFILES

---
+++ block5-profiles/src/main/java/com/example/block5profiles/RunIntegration.java
@@ -0,0 +1,21 @@
+package com.example.block5profiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+@Configuration
+@Profile("INTEGRATION")
+public class RunIntegration implements CommandLineRunner{
+
+    @Value("${bd.url}")
+    private String bdURL;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(bdURL);
+    }
+
+}
---
+++ block5-profiles/src/main/java/com/example/block5profiles/RunLocal.java
@@ -0,0 +1,19 @@
+package com.example.block5profiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+@Configuration
+@Profile("LOCAL")
+public class RunLocal implements CommandLineRunner{
+    @Value("${bd.url}")
+    private String bdURL;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(bdURL);
+    }
+}
