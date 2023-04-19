56210d629a32be8f667516036a29508c6952fa49
MartaRondan96 <martarondan96@gmail.com>
2023-04-19 11:48:24 +0200

    my.log

---
+++ Block5-logging/src/main/java/com/example/Block5logging/MainController.java
@@ -0,0 +1,22 @@
+package com.example.Block5logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    Logger log = LoggerFactory.getLogger(MainController.class);
+    @RequestMapping("/")
+    public String logs(){
+        log.trace("Mensaje TRACE");
+        log.debug("Mensaje DEBUG");
+        log.info("Mensaje INFO");
+        log.warn("Mensaje WARNING");
+        log.error("ERROR e");
+
+        return "Ejercicio bloque 5 logging. Mira los logs";
+    }
+}
