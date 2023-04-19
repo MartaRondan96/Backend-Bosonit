ff0a70795049276a5f61806ba84c963c5f2f51df
MartaRondan96 <martarondan96@gmail.com>
2023-04-19 09:20:37 +0200

    ejemplo Logging

---
+++ Practicas/practicaLogging/src/main/java/com/example/practicaLogging/PracticaLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaLoggingApplication.class, args);
+	}
+
+}
---
+++ Practicas/practicaLogging/src/test/java/com/example/practicaLogging/PracticaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
