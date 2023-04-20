2d9fe059f43d1e1db11fe59649aa1e69501804f5
MartaRondan96 <martarondan96@gmail.com>
2023-04-20 11:57:42 +0200

    ejemplo anotaciones

---
+++ Practicas/Spring-anotaciones/src/main/java/com/example/Springanotaciones/SpringAnotacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springanotaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnotacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnotacionesApplication.class, args);
+	}
+
+}
---
+++ Practicas/Spring-anotaciones/src/test/java/com/example/Springanotaciones/SpringAnotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springanotaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
