89011f065c863c746f1218b5a017834b7fde93a7
MartaRondan96 <martarondan96@gmail.com>
2023-04-24 16:18:05 +0200

    block6 person controllers TERMINADO

--- block6-person-controllers/src/main/java/com/example/block6personcontrollers/models/Ciudad.java
+++ block6-person-controllers/src/main/java/com/example/block6personcontrollers/models/Ciudad.java
@@ -2,9 +2,11 @@ package com.example.block6personcontrollers.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
+@Component
 public class Ciudad {
     String nombre;
     int numHabitantes;
