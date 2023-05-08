45adaede3738ffc613e18da503f37c31896d11e0
MartaRondan96 <martarondan96@gmail.com>
2023-05-08 11:38:27 +0200

    Block 7 CRUD-Validation Parte 3: repository

---
+++ block7-crud-validation/src/main/java/com/example/block7crudvalidation/repository/Alumnos_EstudiosRepository.java
@@ -0,0 +1,7 @@
+package com.example.block7crudvalidation.repository;
+
+import com.example.block7crudvalidation.domain.Alumnos_Estudios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Alumnos_EstudiosRepository extends JpaRepository<Alumnos_Estudios,Integer> {
+}
