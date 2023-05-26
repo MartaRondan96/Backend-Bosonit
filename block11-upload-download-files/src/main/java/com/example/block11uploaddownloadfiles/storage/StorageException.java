36de73ce0ba19eb77dfb1cd1a792e150aee44b99
MartaRondan96 <martarondan96@gmail.com>
2023-05-26 10:29:17 +0200

    Block11-upload-download-files storage

---
+++ block11-upload-download-files/src/main/java/com/example/block11uploaddownloadfiles/storage/StorageException.java
@@ -0,0 +1,11 @@
+package com.example.block11uploaddownloadfiles.storage;
+
+public class StorageException extends Throwable {
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
